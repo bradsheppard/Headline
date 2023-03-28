bd8d00fdac89632a583360f24b10ee8e6b22867f
Brad Sheppard <bradsheppard22@gmail.com>
2023-03-27 22:48:40 -0230

    Collector updates

--- collector/main.py
+++ collector/main.py
@@ -1,21 +1,34 @@
+import os
 import grpc
 
 from duckduckgo_search import ddg_news
 from model.result import Result
 from article import article_pb2_grpc, article_pb2
+from interest import interest_pb2_grpc, interest_pb2
 
-interests = ['Metallica', 'Donald Trump']
+backend_service_url = os.environ['BACKEND_URL']
+
+with grpc.insecure_channel(backend_service_url) as channel:
+    stub = interest_pb2_grpc.InterestServiceStub(channel)
+    
+    request = interest_pb2.GetInterestsRequest(
+            userId=1
+    )
+
+    interest_response: interest_pb2.InterestResponse = stub.GetInterests(request)
+    interests = interest_response.interests
 
 
 for interest in interests:
-    responses = ddg_news(interest)
+    search_term = interest.name
+    responses = ddg_news(search_term)
 
     results = []
 
     if responses is None:
         continue
 
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel(backend_service_url) as channel:
         articles = []
 
         for response in responses:
