f218de7755d748595aa2ef72d472259fa6177ad5
Brad Sheppard <bradsheppard22@gmail.com>
2023-07-01 14:06:44 -0230

    Added orchestrator code

---
+++ orchestrator/main.py
@@ -0,0 +1,47 @@
+import os
+import datetime
+
+from google.protobuf.timestamp_pb2 import Timestamp
+
+import grpc
+from kafka import KafkaProducer
+
+from topic import topic_pb2, topic_pb2_grpc
+from collection import collection_pb2, collection_pb2_grpc
+
+
+if __name__ == '__main__':
+    backend_url = os.environ['BACKEND_URL']
+    kafka_host = os.environ['KAFKA_HOST']
+    kafka_topic = os.environ['KAFKA_TOPIC']
+
+    search_time = datetime.datetime.now() + datetime.timedelta(hours=-6)
+    search_time_proto = Timestamp()
+    search_time_proto.FromDatetime(dt=search_time)
+
+    kafka_producer = KafkaProducer(bootstrap_servers=kafka_host)
+
+    with grpc.insecure_channel(backend_url) as channel:
+        stub = topic_pb2_grpc.TopicServiceStub(channel)
+
+        request = topic_pb2.GetPendingTopicsRequest(
+                lastUpdated=search_time_proto
+        )
+
+        topicResponse: topic_pb2.TopicResponse
+        for topicResponse in stub.GetPendingTopics(request):
+            topics = []
+
+            for topic in topicResponse.topics:
+                topics.append(topic.name)
+
+            print(f'Orchestrating topics: {topics}')
+            collection = collection_pb2.Collection(
+                    topics=topics
+            )
+
+            message = collection.SerializeToString()
+
+            future = kafka_producer.send(kafka_topic, value=message)
+            future.get(timeout=60)
+
---
+++ orchestrator/topic/topic_pb2.pyi
@@ -0,0 +1,50 @@
+from google.protobuf import empty_pb2 as _empty_pb2
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class AddTopicsRequest(_message.Message):
+    __slots__ = ["topics", "userId"]
+    TOPICS_FIELD_NUMBER: _ClassVar[int]
+    USERID_FIELD_NUMBER: _ClassVar[int]
+    topics: _containers.RepeatedCompositeFieldContainer[Topic]
+    userId: int
+    def __init__(self, topics: _Optional[_Iterable[_Union[Topic, _Mapping]]] = ..., userId: _Optional[int] = ...) -> None: ...
+
+class GetPendingTopicsRequest(_message.Message):
+    __slots__ = ["lastUpdated"]
+    LASTUPDATED_FIELD_NUMBER: _ClassVar[int]
+    lastUpdated: _timestamp_pb2.Timestamp
+    def __init__(self, lastUpdated: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ...) -> None: ...
+
+class GetTopicsRequest(_message.Message):
+    __slots__ = ["userId"]
+    USERID_FIELD_NUMBER: _ClassVar[int]
+    userId: int
+    def __init__(self, userId: _Optional[int] = ...) -> None: ...
+
+class RemoveTopicsRequest(_message.Message):
+    __slots__ = ["topicNames", "userId"]
+    TOPICNAMES_FIELD_NUMBER: _ClassVar[int]
+    USERID_FIELD_NUMBER: _ClassVar[int]
+    topicNames: _containers.RepeatedScalarFieldContainer[str]
+    userId: int
+    def __init__(self, topicNames: _Optional[_Iterable[str]] = ..., userId: _Optional[int] = ...) -> None: ...
+
+class Topic(_message.Message):
+    __slots__ = ["lastUpdated", "name"]
+    LASTUPDATED_FIELD_NUMBER: _ClassVar[int]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    lastUpdated: _timestamp_pb2.Timestamp
+    name: str
+    def __init__(self, name: _Optional[str] = ..., lastUpdated: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ...) -> None: ...
+
+class TopicResponse(_message.Message):
+    __slots__ = ["topics"]
+    TOPICS_FIELD_NUMBER: _ClassVar[int]
+    topics: _containers.RepeatedCompositeFieldContainer[Topic]
+    def __init__(self, topics: _Optional[_Iterable[_Union[Topic, _Mapping]]] = ...) -> None: ...
